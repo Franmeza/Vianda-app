c1bf4f8b6a15d7b34110a6a4f8926fccd08bcf40
FranMeza <ing.meza.francisco@gmail.com>
2025-07-23 14:57:01 -0600

    Enhance image remote patterns in next.config.js to include additional sources

--- next.config.js
+++ next.config.js
@@ -1,15 +1,64 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
+  images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'res.cloudinary.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'plus.unsplash.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'badun.nestle.es',
+          port: '',
+          pathname: '/**',
+        }, {
+          protocol: 'https',
+          hostname: 'i.pinimg.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'thestayathomechef.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
             protocol: 'https',
-            hostname: 'res.cloudinary.com',
+            hostname: 'pedrosuarezysusrecetas.es',
             port: '',
             pathname: '/**',
-          },
-        ],
-      },
+        },
+        {
+            protocol: 'https',
+            hostname: 'misrecetasdecocina.net',
+            port: '',
+            pathname: '/**',
+        },
+        {
+            protocol: 'https',
+            hostname: 'img-global.cpcdn.com',
+            port: '',
+            pathname: '/**',
+        },
+          
+      
+      ],
+    },
 }
 
 module.exports = nextConfig
